f28ef02948177fdada5a6908149e460ea5df0550
josuered4 <josuered4@gmail.com>
2023-11-24 17:53:34 -0600

    integracion de los estados iniciales

--- app/src/main/java/com/example/rectificadoragarza/ui/rgServices/ServicesFragment.kt
+++ app/src/main/java/com/example/rectificadoragarza/ui/rgServices/ServicesFragment.kt
@@ -5,9 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
-import com.example.rectificadoragarza.R
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.example.rectificadoragarza.databinding.FragmentServicesBinding
+import kotlinx.coroutines.launch
 
 class ServicesFragment : Fragment() {
     private var _binding:FragmentServicesBinding? = null;
@@ -22,5 +26,39 @@ class ServicesFragment : Fragment() {
         _binding = FragmentServicesBinding.inflate(layoutInflater, container, false);
         return binding.root;
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initUI();
+    }
+
+    private fun initUI() {
+        initUIState();
+    }
+
+    private fun initUIState() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED){
+                servicesViewModel.state.collect{
+                    when(it){
+                        ServicesState.Loading -> loadingState();
+                        is ServicesState.Error -> errorState();
+                        is ServicesState.Success -> successState();
+                    }
+                }
+            }
+        }
+    }
+
+    private fun loadingState() {
+        binding.layoutLoading.isVisible = true;
+    }
+    private fun errorState() {
+    }
+    private fun successState() {
+    }
+
+
+
 
 }
--- app/src/main/java/com/example/rectificadoragarza/ui/rgServices/ServicesViewModel.kt
+++ app/src/main/java/com/example/rectificadoragarza/ui/rgServices/ServicesViewModel.kt
@@ -2,8 +2,12 @@ package com.example.rectificadoragarza.ui.rgServices
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class ServicesViewModel @Inject constructor():ViewModel(){
+    private var _state = MutableStateFlow<ServicesState>(ServicesState.Loading); //stado inical
+    val state:StateFlow<ServicesState> = _state; //mandamos un estado inicial
 }
